e486081092f5ff6a80157557f4274feea3556331
KairosTec <94354260+KairosTec@users.noreply.github.com>
2024-03-06 11:12:03 +0200

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,8 @@
 /*!
 
 =========================================================
-* Black Dashboard React v1.2.2
+* Elite Pos v1.0.0
 =========================================================
-
-* Product Page: https://www.creative-tim.com/product/black-dashboard-react
-* Copyright 2023 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/black-dashboard-react/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
 
 =========================================================
 
